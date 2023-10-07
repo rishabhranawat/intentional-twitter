70371e88ff03e81b86270b4e840eef12c5a44883
rishabhranawat <rishabhranawat12345@gmail.com>
2023-10-07 15:15:42 -0700

    initial

--- server/server.py
+++ server/server.py
@@ -8,17 +8,16 @@ import warnings
 # Third-party library imports
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
-from PIL import Image
 import openai
-from dotenv import load_dotenv
 
 # Local application/library specific imports
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 from llama_index import ServiceContext, set_global_service_context
 from llama_index.llms import OpenAI
+os.environ["OPENAI_API_KEY"] = "..."
 
 # Load environment variables
-load_dotenv()
+# load_dotenv()
 
 # app settings
 app = Flask(__name__)
@@ -30,9 +29,9 @@ cors = CORS(app, resource={
 })
 
 # LLM Settings
-# llm = OpenAI(model="gpt-4", temperature=0, max_tokens=256)
-# service_context = ServiceContext.from_defaults(llm=llm)
-service_context = ServiceContext.from_defaults(llm="local")
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0, max_tokens=256)
+service_context = ServiceContext.from_defaults(llm=llm)
+# service_context = ServiceContext.from_defaults(llm="local")
 set_global_service_context(service_context)
 
 documents = SimpleDirectoryReader('data').load_data()
