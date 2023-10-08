e9e408ed9f347c503a06900c2512d14c6409c817
rishabhranawat <rishabhranawat12345@gmail.com>
2023-10-07 17:10:01 -0700

    prompting + pinecone RAG flow

--- ingester/ingester_arxiv.py
+++ ingester/ingester_arxiv.py
@@ -5,6 +5,9 @@ import pinecone
 import openai
 import numpy as np
 
+_TOPIC = "Quantum Computing"
+_TOPIC_KEY = _TOPIC.replace(" ", "_")
+
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 pinecone.init(api_key=os.environ["PINECONE_API_KEY"],
@@ -17,16 +20,24 @@ def get_embedding(text, model="text-embedding-ada-002"):
 
 # Open the file in write mode
 search = arxiv.Search(
-	query="Large Language Models and RAGS",
-	max_results=10000,
+	query=_TOPIC,
+	max_results=50,
 	sort_by=arxiv.SortCriterion.SubmittedDate
 )
 
 data_arr = []
-for i, result in enumerate(search.results()):
-	authors = ','.join([x.name for x in result.authors])
-	data = f'Title: {result.title}\nAuthors: {authors}\nSummary: {result.summary}\nPublished On: {result.published}\nCategory: {result.primary_category}\n\n'
-	embedding = get_embedding(data)
-	data_arr.append((str(i), embedding, {"title": result.title, "summary": result.summary, "authors": authors}))
+try:
+	for i, result in enumerate(search.results()):
+		try:
+			print(f"Ingesting result {i}")
+			authors = ','.join([x.name for x in result.authors])
+			data = f'Title: {result.title}\nAuthors: {authors}\nSummary: {result.summary}\nPublished On: {result.published}\nCategory: {result.primary_category}\n\n'
+			embedding = get_embedding(data)
+			data_arr.append((str(i)+_TOPIC_KEY, embedding, {"title": result.title, "url": result.pdf_url, "authors": authors, "summary": result.summary}))
+		except Exception as e:
+			print(f"Error processing result {i}: {e}")
+			continue
+except Exception as ea:
+	print(f"Error processing arxiv result: {ea}")
 
 index.upsert(vectors=data_arr)
--- server/server.py
+++ server/server.py
@@ -11,8 +11,13 @@ from flask_cors import CORS
 import openai
 import pinecone
 
+# constants
+OPEN_AI_CHAT_MODEL = 'gpt-3.5-turbo'
+
+# open ai set up
 openai.api_key = os.environ['OPENAI_API_KEY']
 
+# pinecone set up
 pinecone.init(api_key=os.environ["PINECONE_API_KEY"],
 			  environment=os.environ["PINECONE_ENVIRONMENT"])
 index = pinecone.Index("arxiv-index-v2")
@@ -25,6 +30,24 @@ cors = CORS(app, resource={
 		"origins":"*"
 	}
 })
+
+def get_gpt_response(content):
+	chat_completion = openai.ChatCompletion.create(
+		model=OPEN_AI_CHAT_MODEL, 
+		messages=[{"role": "user", "content": content}])
+	return chat_completion.choices[0].message.content
+
+def get_chatgpt_prompt(query, top_k):
+	prompt = f"You are restricted to the data provided in this context. The original query is this: {query}."
+	prompt += "Based on the following research author and summary recommend who the author should talk to: \n"
+	for each in top_k:
+		if 'summary' not in each.metadata: continue
+		prompt += f"Title: {each.metadata['title']}; Authors: {each.metadata['authors']}; Summary: {each.metadata['summary']}; url:{each.metadata['url']} \n \n"
+
+	prompt += "Be concise and generate an insightful but factual response."
+	prompt += " The structure of the response should be: Title, Author, Url, why they should connect in bullet points. Break it up per relevant research paper."
+	return prompt
+
 
 def get_embedding(text, model="text-embedding-ada-002"):
    text = text.replace("\n", " ")
@@ -45,17 +68,15 @@ def chat():
 	data = request.get_json()
 	query = data['query']
 	embedding = get_embedding(query)
-
 	nn = index.query(
 		  vector=embedding,
 		  top_k=3,
 		  include_values=True,
 		  include_metadata=True
 	)
-	for each in nn.matches:
-		print(each.metadata)
-
-	return jsonify({"response": "success"})
+	prompt = get_chatgpt_prompt(query, nn.matches)
+	print(f'Generated Prompt: {prompt}')	
+	return jsonify({"response": get_gpt_response(prompt)})
 
 if __name__ == "__main__":
 	app.run(debug=True)
